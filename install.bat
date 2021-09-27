e4c6eb652f29872296bd693242a955d22198533c
egg-hamada <86991695+egg-hamada@users.noreply.github.com>
2021-09-27 14:53:14 +0900

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,13 @@
+REM [ Firefoxは英語版が入ってしまうためここに記述していません ]
+winget install -e --id Google.Chrome
+winget install -e --id TeraTermProject.teraterm
+winget install -e --id WinSCP.WinSCP
+winget install -e --id PuTTY.PuTTY
+winget install -e --id Oracle.VirtualBox
+winget install -e --id Hashicorp.Vagrant
+winget install -e --id JetBrains.Toolbox
+winget install -e --id JetBrains.PHPStorm
+winget install -e --id Git.Git
+winget install -e --id TortoiseGit.TortoiseGit
+winget install -e --id PostgreSQL.pgAdmin-x64
+winget install -e --id 7zip.7zip
