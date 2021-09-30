c989bbdd4408f86510b3a6248e5fc8958608741c
egg-hamada <86991695+egg-hamada@users.noreply.github.com>
2021-09-30 18:30:22 +0900

    Update winget.bat

--- winget.bat
+++ winget.bat
@@ -1,20 +1,30 @@
 REM [ -i はインストールウィザード画面を表示するオプション。ないとエラーになるケースや個別に設定が必要なケースで使用 ]
 REM [ -e --id は大文字小文字を区別してアプリのIDを指定するというオプション ]
 REM [ Firefoxは英語版が入ってしまうためここに記述していません ]
-winget install -e --id Google.Chrome
-winget install -i -e --id Microsoft.Office
-winget install -e --id TeraTermProject.teraterm
-winget install -e --id WinSCP.WinSCP
-winget install -e --id PuTTY.PuTTY
-winget install -i -e --id Oracle.VirtualBox
-winget install -e --id Hashicorp.Vagrant
-winget install -e --id JetBrains.Toolbox
-winget install -e --id PostgreSQL.pgAdmin
-winget install -e --id Postman.Postman
-winget install -e --id 7zip.7zip
+
 start https://github.com/egg-co-jp/install_windows_app/blob/main/doc/git_install_manual.md
 winget install -i -e --id Git.Git
 winget install -e --id GitHub.GitLFS
 winget install -e --id Microsoft.GitCredentialManagerCore
 winget install -e --id TortoiseGit.TortoiseGit
+
+winget install -i -e --id Oracle.VirtualBox
+winget install -e --id Hashicorp.Vagrant
+
+winget install -e --id TeraTermProject.teraterm
+winget install -e --id WinSCP.WinSCP
+winget install -e --id PuTTY.PuTTY
+
+winget install -e --id JetBrains.Toolbox
+
+winget install -e --id PostgreSQL.pgAdmin
+winget install -e --id Postman.Postman
+
+winget install -e --id 7zip.7zip
+winget install -e --id Google.Chrome
+winget install -i -e --id Microsoft.Office
+
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Notepad++.Notepad++
+
 start https://github.com/egg-co-jp/install_windows_app/blob/main/doc/after_winget.md
