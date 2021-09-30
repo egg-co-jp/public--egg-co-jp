f819f657031a1e932c9b55182fc53cc9e069c6bb
egg-hamada <86991695+egg-hamada@users.noreply.github.com>
2021-09-30 16:32:53 +0900

    Update winget.bat

--- winget.bat
+++ winget.bat
@@ -1,5 +1,6 @@
 REM [ Firefoxは英語版が入ってしまうためここに記述していません ]
 winget install -e --id Google.Chrome
+winget install -i -e --id Microsoft.Office
 winget install -e --id TeraTermProject.teraterm
 winget install -e --id WinSCP.WinSCP
 winget install -e --id PuTTY.PuTTY
@@ -7,10 +8,11 @@ winget install -e --id Oracle.VirtualBox
 winget install -e --id Hashicorp.Vagrant
 winget install -e --id JetBrains.Toolbox
 winget install -e --id JetBrains.PHPStorm
+winget install -e --id Microsoft.GitCredentialManagerCore
+winget install -e --id PostgreSQL.pgAdmin
+winget install -e --id Postman.Postman
+winget install -e --id 7zip.7zip
 start https://github.com/egg-co-jp/install_windows_app/blob/main/doc/git_install_manual.md
 winget install -i -e --id Git.Git
 winget install -e --id GitHub.GitLFS
 winget install -e --id TortoiseGit.TortoiseGit
-winget install -e --id Microsoft.GitCredentialManagerCore
-winget install -e --id PostgreSQL.pgAdmin
-winget install -e --id 7zip.7zip
