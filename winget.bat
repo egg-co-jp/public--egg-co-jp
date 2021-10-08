b91303b4e9ec058f4ae601cbc1bd772ed3f1cb31
egg-hamada <86991695+egg-hamada@users.noreply.github.com>
2021-10-08 14:07:29 +0900

    Update winget.bat

--- winget.bat
+++ winget.bat
@@ -2,28 +2,35 @@ REM [ -i はインストールウィザード画面を表示するオプショ�
 REM [ -e --id は大文字小文字を区別してアプリのIDを指定するというオプション ]
 REM [ Firefoxは英語版が入ってしまうためここに記述していません ]
 
+REM [ GIT関連 ]
 start https://github.com/egg-co-jp/install_windows_app/blob/main/doc/git_install_manual.md
 winget install -i -e --id Git.Git
 winget install -e --id GitHub.GitLFS
 winget install -e --id Microsoft.GitCredentialManagerCore
 winget install -e --id TortoiseGit.TortoiseGit
+winget install -e --id WinMerge.WinMerge
 
+REM [ 仮想マシン ]
 winget install -i -e --id Oracle.VirtualBox
 winget install -e --id Hashicorp.Vagrant
 
+REM [ ユーティリティ ]
 winget install -e --id TeraTermProject.teraterm
 winget install -e --id WinSCP.WinSCP
 winget install -e --id PuTTY.PuTTY
 
+REM [ 開発 ]
 winget install -e --id JetBrains.Toolbox
 
 winget install -e --id PostgreSQL.pgAdmin
 winget install -e --id Postman.Postman
 
+REM [ 一般 ]
 winget install -e --id 7zip.7zip
 winget install -e --id Google.Chrome
 winget install -i -e --id Microsoft.Office
 
+REM [ その他 ]
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Notepad++.Notepad++
 
