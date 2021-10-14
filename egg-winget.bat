948122478d1d0db475d38f5fb66ea78a364a95bd
hamada.y <hamada.y@egg.co.jp>
2021-10-14 11:43:08 +0900

    [main]-[fix]: PATH問題いったん元に戻す

--- egg-winget.bat
+++ egg-winget.bat
@@ -5,7 +5,7 @@ REM [ -e --id は大文字小文字を区別してアプリのIDを指定する�
 REM [ Firefoxは英語版が入ってしまうためここに記述していません ]
 
 REM [ 管理者権限で起動するとPATHが通ってないので、一時的にPATHを通します ]
-SET PATH=%PATH%;C:%HOMEPATH%\AppData\Local\Microsoft\WindowsApps
+@REM SET PATH=%PATH%;C:%HOMEPATH%\AppData\Local\Microsoft\WindowsApps
 
 REM [ GIT関連 ]
 start https://github.com/egg-co-jp/install_windows_app/blob/main/doc/git_install_manual.md
