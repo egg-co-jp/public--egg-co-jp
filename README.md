41f034cfdd1b9d6b72834dd6d3b97011562bc3ed
hamada.y <hamada.y@egg.co.jp>
2022-02-01 14:15:51 +0900

    [master]-[update]:

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # install_windows_app
 Windowsアプリを「winget」でインストールします。
 
-まずは [winget（Windows Package Manager）](https://github.com/microsoft/winget-cli/releases/latest) をインストールしてください。
-（拡張子が .msixbundle のファイルをDL、実行）
+まずは [Microsoft Store で 「アプリインストーラ」](https://www.microsoft.com/ja-jp/p/app-installer/9nblggh4nns1) をインストールしてください。
+このアプリにwingetコマンドが含まれています。
 
-管理者権限は不要です。
-
-![image](https://user-images.githubusercontent.com/86991695/135211970-0af80bb2-8bcd-49e7-a527-d85d225c1c56.png)
-
-Githubページの「Code」メニューからDownload ZIPしてください。
+次に本Githubページの「Code」メニューからDownload ZIPしてください。
 
 ![ダウンロード](https://user-images.githubusercontent.com/86991695/134862588-27c6996f-51aa-4892-b282-c36e9a73df4f.png)
 
--- egg-winget.bat
+++ egg-winget.bat
@@ -1,17 +1,23 @@
-REM [ ！！ CRLFであることを確認 LFだと不具合を起こします ！！ ]
+REM [ ★ 管理者として実行ではなく、普通に起動してください ]
+REM [ ★ CRLFであることを確認 LFだと不具合を起こします ]
 
 REM [ -i はインストールウィザード画面を表示するオプション。ないとエラーになるケースや個別に設定が必要なケースで使用 ]
 REM [ -e --id は大文字小文字を区別してアプリのIDを指定するというオプション ]
 REM [ Firefoxは英語版が入ってしまうためここに記述していません ]
 
-REM [ (工事中) winget PATHがうまくいかない問題。管理者として実行ではなく、普通に起動してください ]
+
+REM [ 一般 ]
+start "" cmd /c "echo Officeのインストールは数分かかります。しばらく待機してください。このウインドウは閉じて問題ありません。&echo(&pause"
+winget install -e --id Microsoft.Office
+winget install -e --id 7zip.7zip
+winget install -e --id Google.Chrome
 
 REM [ GIT関連 ]
 start https://github.com/egg-co-jp/install_windows_app/blob/main/doc/git_install_manual.md
 winget install -i -e --id Git.Git
 winget install -e --id GitHub.GitLFS
 winget install -e --id Microsoft.GitCredentialManagerCore
-winget install -e --id TortoiseGit.TortoiseGit
+winget install -i -e --id TortoiseGit.TortoiseGit
 winget install -e --id WinMerge.WinMerge
 
 REM [ 仮想マシン ]
@@ -28,11 +34,6 @@ winget install -e --id JetBrains.Toolbox
 
 winget install -e --id PostgreSQL.pgAdmin
 winget install -e --id Postman.Postman
-
-REM [ 一般 ]
-winget install -e --id 7zip.7zip
-winget install -e --id Google.Chrome
-winget install -i -e --id Microsoft.Office
 
 REM [ その他 ]
 winget install -e --id Microsoft.WindowsTerminal
