fc1e78d89c8a8ef4020b9dfafe0736eb7331203c
egg-hamada <hamada.y@egg.co.jp>
2023-03-16 07:46:36 +0900

    github cli

--- install_windows_app/egg-winget.bat
+++ install_windows_app/egg-winget.bat
@@ -18,6 +18,7 @@ winget install -i -e --id Git.Git
 winget install -e --id GitHub.GitLFS
 winget install -i -e --id TortoiseGit.TortoiseGit
 winget install -e --id WinMerge.WinMerge
+winget install -e --id GitHub.cli --accept-source-agreements --force
 
 REM [ ‰¼‘zƒ}ƒVƒ“ ]
 winget install -i -e --id Oracle.VirtualBox
