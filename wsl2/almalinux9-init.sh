97b136f34cfb3041aab1a93210187a5a002a79fa
egg-hamada <hamada.y@egg.co.jp>
2023-05-12 09:50:34 +0900

    Update almalinux9-init.sh

--- wsl2/almalinux9-init.sh
+++ wsl2/almalinux9-init.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
-# phpstormからgit操作するため git と言語パックが必要
+# エイリアスを有効にする
+shopt -s expand_aliases
+source ~/.bash_profile
+
+echo -e "\n phpstormからgit操作するため git と言語パックをインストール \n"
 sudo dnf install -y git glibc-langpack-en
 
-# Laravel用にエイリアスを登録。 vendor/bin/sail を sail だけで起動できるようにする
+echo -e "\n Laravel用にエイリアスを登録。 vendor/bin/sail を sail だけで起動できるようにする \n"
 if ! command -v sail &> /dev/null; then echo "alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'" >> ~/.bash_profile && source ~/.bash_profile; fi 2> /dev/null
 source ~/.bash_profile
 
 
-# WSL と Windows 連携を便利にする wslu を導入
+echo -e "\n WSL と Windows 連携を便利にする wslu を導入 \n"
 sudo dnf install -y epel-release 
 sudo dnf copr enable -y wslutilities/wslu
 sudo dnf install -y wslu
 
-# Windows側のGit認証情報を利用するように設定する
+echo -e "\n Windows側のGit認証情報を利用するように設定する \n";
 WINGIT_PATH=$(where.exe git | grep mingw | sed -z 's/\r\n//g' | xargs -0 -I {} wslpath -u "{}")
 WINCRED_PATH=$(realpath "$(dirname "$WINGIT_PATH")/../")/libexec/git-core/git-credential-wincred.exe
 git config --global credential.helper "$(echo "$WINCRED_PATH" | sed -e 's/ /\\ /g')"
