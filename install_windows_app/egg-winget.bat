deda32fe6e1bd0915d5c253f9832a01bb6e3bbed
egg-hamada <hamada.y@egg.co.jp>
2024-05-08 08:52:36 +0900

    update egg-winget.bat

--- install_windows_app/egg-winget.bat
+++ install_windows_app/egg-winget.bat
@@ -1,6 +1,6 @@
 @echo off
 REM [ ★ 管理者として実行ではなく、普通に起動してください ]
-REM [ ★ CRLFであることを確認 LFだと不具合を起こします ]
+REM [ ★ CRLFであることを確認 LF だと不具合を起こします ]
 
 REM [ -i はインストールウィザード画面を表示するオプション。ないとエラーになるケースや個別に設定が必要なケースで使用 ]
 REM [ -e --id は大文字小文字を区別してアプリのIDを指定するというオプション ]
@@ -11,8 +11,9 @@ REM [ 一般 ]
 winget install -e --id 7zip.7zip
 winget install -e --id Google.Chrome
 winget install -e --id Mozilla.Firefox
+winget install -e --id Zoom.Zoom
 
-REM [ GIT関連 ]
+REM [ Git関連 ]
 winget install -i -e --id Git.Git
 winget install -e --id GitHub.GitLFS
 winget install -i -e --id TortoiseGit.TortoiseGit
@@ -30,9 +31,8 @@ winget install -e --id PuTTY.PuTTY
 winget install --source msstore --accept-package-agreements DevToys
 
 REM [ 開発 ]
-winget install -e --id JetBrains.Toolbox
+winget install -i -e --id JetBrains.Toolbox
 winget install -e --id Microsoft.VisualStudioCode
-
 winget install -e --id PostgreSQL.pgAdmin
 winget install -e --id Postman.Postman
 
